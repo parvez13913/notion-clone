333256c6df8826768b90f79312f00585adf51db9
parvez13913 <parvezz13913@gmail.com>
2024-09-10 12:29:52 +0600

    [DONE] Authentication with convex

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,7 +5,12 @@ import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -16,6 +21,20 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <p>Loading....</p>}
+        {!isLoading && !isAuthenticated && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal">
+              <Button size="sm">Get potion free</Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
