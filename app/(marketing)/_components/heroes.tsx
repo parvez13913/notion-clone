6a4a51c5814bf1e3c7aafb73ec6e04cf362bdb69
parvez13913 <parvezz13913@gmail.com>
2024-09-12 12:42:35 +0600

    Heroes component updated

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -6,17 +6,31 @@ const Heroes = () => {
       <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
           <Image
-            src="/documents.svg"
+            src="/documents.png"
             fill
             className="object-contain"
+            alt="Documents"
+          />
+
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Documents"
           />
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
           <Image
-            src="/reading.svg"
+            src="/reading.png"
             fill
             className="object-contain"
+            alt="Reading"
+          />
+
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Reading"
           />
         </div>
