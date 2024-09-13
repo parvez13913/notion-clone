d8cf51fa8ede1302c0f9e3d6db1a5fb9cb761d8a
parvez13913 <parvezz13913@gmail.com>
2024-09-13 15:01:14 +0600

    Layout code modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Potion",
@@ -38,6 +39,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="potion-theme-2"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
