0c95eba7980f70ba6f8cd1e1a8c50e26111f8bf8
parvez13913 <parvezz13913@gmail.com>
2024-09-14 20:20:09 +0600

    Documents code updated

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,23 @@
 import { v } from 'convex/values'
-import { mutation } from './_generated/server'
+import { mutation, query } from './_generated/server'
 
+
+
+
+
+export const get = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const documents = await ctx.db.query("documents");
+
+    return documents;
+  }
+})
 
 export const create = mutation({
   args: {
