dca810976cd43936130c01aad68ae603eba82fd2
parvez13913 <parvezz13913@gmail.com>
2024-09-25 16:31:33 +0600

    [DONE] toolBar and icon-picker component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,36 @@
-const DocumentIdPage = () => {
-  return <div>Document</div>;
+"use client";
+
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params?.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>loading...</div>;
+  }
+  if (document === null) {
+    return <div>not found.</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
