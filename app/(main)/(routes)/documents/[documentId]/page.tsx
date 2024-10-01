35184c25aec3361835c995bb4279f7f411f5f838
parvez13913 <parvezz13913@gmail.com>
2024-10-01 20:27:38 +0600

    [DONE] Add cover image

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Cover from "@/components/cover";
 import Toolbar from "@/components/toolbar";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -25,6 +26,7 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
 
   return (
     <div className="pb-40">
+      <Cover url={document?.coverImage} />
       <div className="h-[35vh]" />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
---
+++ components/cover.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { UseCoverImage } from "@/hooks/use-cover-image";
+
+interface CoverImageProps {
+  url?: string;
+  preview?: string;
+}
+
+const Cover = ({ url, preview }: CoverImageProps) => {
+  const coverImage = UseCoverImage();
+  return (
+    <div
+      className={cn(
+        "relative w-full h-[35vh] group",
+        !url && "h-[12vh]",
+        url && "bg-muted"
+      )}
+    >
+      {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
+
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change cover
+          </Button>
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X className="h-4 w-4 mr-2" />
+            Remove
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cover;
