ef78f513f89cfb62fd1dabcd5a0679c64ea288d0
parvez13913 <parvezz13913@gmail.com>
2024-10-04 06:48:27 +0600

    [DONE] Skeleton added

--- components/cover.tsx
+++ components/cover.tsx
@@ -11,6 +11,7 @@ import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 interface CoverImageProps {
   url?: string;
@@ -71,3 +72,7 @@ const Cover = ({ url, preview }: CoverImageProps) => {
 };
 
 export default Cover;
+
+Cover.Skeleton = function CoverSkeleton() {
+  return <Skeleton className="w-full h-[12vh]" />;
+};
